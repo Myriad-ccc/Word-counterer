914e869a512f74e0d5619a5f8c7e517949e54ceb
Myriad-ccc <off7cal@gmail.com>
2025-03-30 18:53:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Word_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File path: ");
+            string path = Console.ReadLine();
+            Console.WriteLine("\nWords in file by frequency: \n");
+
+            var words = File.ReadAllText(path).
+                Split(new char[] { ',', ' ', '!', '.', ';', ':', '?', '#', '@' }, StringSplitOptions.RemoveEmptyEntries).
+                Where(x => !string.IsNullOrEmpty(x) && x.All(char.IsLetter)).
+                Select(x => x.ToLower());
+
+            var wordCount = words.GroupBy(word => word).
+                OrderByDescending(group => group.Count());
+
+            foreach (var group in wordCount)
+            {
+                Console.WriteLine($"{group.Key} was found {group.Count()} times");
+            }
+        }
+    }
+}
